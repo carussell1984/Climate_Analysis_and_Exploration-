445a4df68f9d6b6d0e9cbe552f23ec8623c1f3cb
Carly Russell <c.a.movius@gmail.com>
2019-08-01 11:29:38 -0700

    updated comments to app.py

--- Flask/app.py
+++ Flask/app.py
@@ -44,6 +44,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+   #code below has available routes, with explainations of what will be returned, and instructions for performing temp data searches by date
+   #also includes some formating
    return (
                 f"<strong>Available Routes:</strong><br/>"
                 f"<br/>"
@@ -75,7 +77,6 @@ def prcp():
     prcp_data = session.query(Measurement.date, Measurement.station, Measurement.prcp).all()
 
     #create a dictionary from the new raw data and append to a list of prcp, with, station, and date values
-
     all_prcp = []
     for date, station, prcp in prcp_data:
         prcp_dict = {}
@@ -89,7 +90,7 @@ def prcp():
 
 @app.route("/api/v1.0/stations")
 def stations():
-    # Query all dates and precipitation
+    # Query all stations & return in a jsonified format
     session = Session(engine)
     stations = session.query(Station.station).all()
 
@@ -101,12 +102,13 @@ def stations():
 def tobs():
 
 
-
+    # Query all temperature observations within a year of the last date in the data set, including the station for which the temperature obs was recorded at
     session = Session(engine)
 
     tobs_data = session.query(Measurement.date, Measurement.tobs, Measurement.station).\
             filter(Measurement.date >= "2016-08-23").all()
 
+    #Create a dictionary of results
     all_tobs = []
     for date, tobs, station in tobs_data:
         tobs_dict = {}
@@ -116,12 +118,14 @@ def tobs():
 
         all_tobs.append(tobs_dict)
 
+    #return the query in a jsonified format
     return jsonify(all_tobs)
 
 
 @app.route("/api/v1.0/<start>")
 def above_date(start):
-    
+
+    #this logic is applied if the user uses incorrect date formating, or misspells stations, or misspells precipitation
     try:    
         
         query_date =  datetime.strptime(start,"%Y-%m-%d").date()
@@ -135,7 +139,7 @@ def above_date(start):
                 f"To receive a list of stations the route is: /api/v1.0/stations<br/>"
                 f"To receive a precipation data the route is: /api/v1.0/precipitation<br/>")
 
-        
+    # the following logic is to remind users to keep search queries within the specified date range, and to follow the required format
     if query_date >= datetime.strptime("2017-08-24","%Y-%m-%d").date():
         return (f"Your entry failed to return a search. Please check your entry and try again.<br/>"
                 f"Please refer to the set of instructions below:<br/>"
@@ -150,14 +154,14 @@ def above_date(start):
                 f"&nbsp&nbsp&nbsp&nbsp&nbsp* Dates must be in YYYY-MM-DD format.<br/>"
                 f"&nbsp&nbsp&nbsp&nbsp&nbsp* Do not preceed or end the date with either  '  or  '""' symbols.")
         
-        
+    #the following is the query to return temp data (max, avg, min) for a date range including the query date and above   
     else: 
         query_date =  datetime.strptime(start,"%Y-%m-%d").date()
         session = Session(engine)
         sel = [func.max(Measurement.tobs), func.avg(Measurement.tobs), func.min(Measurement.tobs)]
         temp_post_date = session.query(*sel).filter(Measurement.date >= query_date).all()
 
-
+        # using a dictionary to store the query output
         temp_data_post = []
         for tmax, tavg, tmin in temp_post_date:
             temp_dict = {}
@@ -166,14 +170,15 @@ def above_date(start):
             temp_dict["Temp (min)"] = tmin
 
             temp_data_post.append(temp_dict)
-
+        
+        #return the data in a jsonified form
         return jsonify(temp_data_post)
     
 
 
 @app.route("/api/v1.0/<start>/<end>")
 def tempbetweendates(start, end):
-
+    #this logic is applied if the user uses incorrect date formating
     try:    
         start_dt = datetime.strptime(start, "%Y-%m-%d").date()
         end_dt = datetime.strptime(end, "%Y-%m-%d").date()
@@ -185,7 +190,7 @@ def tempbetweendates(start, end):
                 f"&nbsp&nbsp&nbsp&nbsp&nbsp* Dates must be in YYYY-MM-DD format.<br/>"
                 f"&nbsp&nbsp&nbsp&nbsp&nbsp* Do not preceed or end the date with either  '  or  '""' symbols.<br/><br/>")
                 
-
+    #the following logic helps ensure that correct formating of dates are entered, and that the dates ranges is in the specs of the data 
     if start_dt > end_dt:
         return (f"start date must be earlier than end date"
                 f"Please refer to the set of instructions below:<br/>"
@@ -221,6 +226,7 @@ def tempbetweendates(start, end):
                 f"&nbsp&nbsp&nbsp&nbsp&nbsp* Dates must be in YYYY-MM-DD format.<br/>"
                 f"&nbsp&nbsp&nbsp&nbsp&nbsp* Do not preceed or end the date with either  '  or  '""' symbols.")
     
+    #begin query for temp date between and including the start and end date that the user provided
     else:
         
         session = Session(engine)
@@ -229,6 +235,7 @@ def tempbetweendates(start, end):
              filter(Measurement.date >= start_dt).\
              filter(Measurement.date <= end_dt).all()
 
+        #create a dictionary to store the results
         temp_data_between = []
         for tmax, tavg, tmin in temp_data:
             temp_dict_2 = {}
@@ -237,11 +244,12 @@ def tempbetweendates(start, end):
             temp_dict_2["Temp (min)"] = tmin
 
             temp_data_between.append(temp_dict_2)
-
+        
+        #results are returned in a jsonified format
         return jsonify(temp_data_between)
 
 
-    
+#debugger is added    
 if __name__ == '__main__':
     app.run(debug=True)
         
