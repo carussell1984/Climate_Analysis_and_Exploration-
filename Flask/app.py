8fc5c64b6ad93a391d97bfae17724920d6ef6080
Carly Russell <c.a.movius@gmail.com>
2019-08-01 10:53:32 -0700

    tested api routes for climate_app

--- Flask/app.py
+++ Flask/app.py
@@ -53,9 +53,11 @@ def home():
                 f"&nbsp&nbsp&nbsp&nbsp&nbsp/api/v1.0/stations:<br/>"
                 f"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp-Will provide a list of stations from the dataset.<br/>"
                 "<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp/api/v1.0/tobs:<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp-Query for the dates and temperature observations from a year from the last data point.<br/>"
                 f"&nbsp&nbsp&nbsp&nbsp&nbsp/api/v1.0/&ltstart&gt:<br/>"
                 f"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp-Query for the dates and temperature observations from a year from the last data point.<br/>"
-                "<br/>"
+                f"<br/>"
                 f"&nbsp&nbsp&nbsp&nbsp&nbsp/api/v1.0/&ltstart&gt/&ltend&gt:<br/>"
                 f"&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp-Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.<br/>")
 
@@ -90,49 +92,150 @@ def stations():
 
     return jsonify(list_stations)
 
+@app.route("/api/v1.0/tobs")
+def tobs():
+
+
+
+    session = Session(engine)
+
+    tobs_data = session.query(Measurement.date, Measurement.tobs, Measurement.station).\
+            filter(Measurement.date >= "2016-08-23").all()
+
+    all_tobs = []
+    for date, tobs, station in tobs_data:
+        tobs_dict = {}
+        tobs_dict["Date"] = date
+        tobs_dict["Temp Obs"] = tobs
+        tobs_dict["Station"] = station
+
+        all_tobs.append(tobs_dict)
+
+    return jsonify(all_tobs)
+
+
 @app.route("/api/v1.0/<start>")
-def date(start):
+def above_date(start):
+    
     try:    
-        start_date = datetime.strptime(start, "%Y-%m-%d")
-        year_before = start_date-timedelta(days=365)
+        
+        query_date =  datetime.strptime(start,"%Y-%m-%d").date()
+    
     except ValueError:
         return (f"Your entry failed to return a search. Please check your entry and try again.<br/>"
-                f"Please refer to the set of instruction below:<br/>"
+                f"Please refer to the set of instructions below:<br/>"
                 f"&nbsp&nbsp&nbsp&nbsp&nbsp* Enter a start date between the dates of '2011-01-01' and '2017-08-23'.<br/>"
                 f"&nbsp&nbsp&nbsp&nbsp&nbsp* Dates must be in YYYY-MM-DD format.<br/>"
-                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Do not preceed or end the date with either  '  or  '""' symbols.")
-    
-    if start_date <= datetime(2017, 8, 23) or start_date >= datetime(2011, 1, 1):
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Do not preceed or end the date with either  '  or  '""' symbols.<br/><br/>"
+                f"To receive a list of stations the route is: /api/v1.0/stations<br/>"
+                f"To receive a precipation data the route is: /api/v1.0/precipitation<br/>")
+
         
-        str_yago = year_before.strftime("%Y-%m-%d")
-
+    if query_date >= datetime.strptime("2017-08-24","%Y-%m-%d").date():
+        return (f"Your entry failed to return a search. Please check your entry and try again.<br/>"
+                f"Please refer to the set of instructions below:<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Enter a start date between the dates of '2010-01-01' and '2017-08-23'.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Dates must be in YYYY-MM-DD format.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Do not preceed or end the date with either  '  or  '""' symbols.")
+        
+    elif query_date <= datetime.strptime("2009-12-31","%Y-%m-%d").date():
+        return (f"Your entry failed to return a search. Please check your entry and try again.<br/>"
+                f"Please refer to the set of instructions below:<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Enter a start date between the dates of '2010-01-01' and '2017-08-23'.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Dates must be in YYYY-MM-DD format.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Do not preceed or end the date with either  '  or  '""' symbols.")
+        
+        
+    else: 
+        query_date =  datetime.strptime(start,"%Y-%m-%d").date()
         session = Session(engine)
-        date_temp_date = session.query(Measurement.date, Measurement.tobs, Measurement.station).\
-            filter(Measurement.date.between(str_yago, start)).all()
-   
-        all_temp = []
-        for date, tobs, station in date_temp_date:
+        sel = [func.max(Measurement.tobs), func.avg(Measurement.tobs), func.min(Measurement.tobs)]
+        temp_post_date = session.query(*sel).filter(Measurement.date >= query_date).all()
+
+
+        temp_data_post = []
+        for tmax, tavg, tmin in temp_post_date:
             temp_dict = {}
-            temp_dict["Date"] = date
-            temp_dict["Temp (*F)"] = tobs
-            temp_dict["Station"] = station
+            temp_dict["Temp (max)"] = tmax
+            temp_dict["Temp (avg)"] = tavg
+            temp_dict["Temp (min)"] = tmin
 
-            all_temp.append(temp_dict)
+            temp_data_post.append(temp_dict)
 
-        return jsonify(all_temp)
-    
-    elif start_date > datetime(2017, 8, 23):
-        return f"start date be between '2011-01-01'and '2017-08-23'"
-
-    elif year_before < datetime(2011, 1, 1):
-        return f"start date be between '2011-01-01'and '2017-08-23'"
+        return jsonify(temp_data_post)
     
 
 
 @app.route("/api/v1.0/<start>/<end>")
-def datebetween(start, end):
-#    session = Session(engine)
-    return "This is in progress start, end"
+def tempbetweendates(start, end):
+
+    try:    
+        start_dt = datetime.strptime(start, "%Y-%m-%d").date()
+        end_dt = datetime.strptime(end, "%Y-%m-%d").date()
+    
+    except ValueError:
+        return (f"Your entry failed to return a search. Please check your entry and try again.<br/>"
+                f"Please refer to the set of instructions below:<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Enter a start date and end between the dates of '2010-01-01' and '2017-08-23'.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Dates must be in YYYY-MM-DD format.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Do not preceed or end the date with either  '  or  '""' symbols.<br/><br/>")
+                
+
+    if start_dt > end_dt:
+        return (f"start date must be earlier than end date"
+                f"Please refer to the set of instructions below:<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Enter a start date and end between the dates of '2010-01-01' and '2017-08-23'.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Dates must be in YYYY-MM-DD format.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Do not preceed or end the date with either  '  or  '""' symbols.<br/><br/>")
+
+    elif start_dt >= datetime.strptime("2017-08-24","%Y-%m-%d").date():
+        return (f"Your entry failed to return a search. Please check your entry and try again.<br/>"
+                f"Please refer to the set of instructions below:<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Enter a start date between the dates of '2010-01-01' and '2017-08-23'.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Dates must be in YYYY-MM-DD format.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Do not preceed or end the date with either  '  or  '""' symbols.")
+        
+    elif start_dt <= datetime.strptime("2009-12-31","%Y-%m-%d").date():
+        return (f"Your entry failed to return a search. Please check your entry and try again.<br/>"
+                f"Please refer to the set of instructions below:<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Enter a start date between the dates of '2010-01-01' and '2017-08-23'.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Dates must be in YYYY-MM-DD format.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Do not preceed or end the date with either  '  or  '""' symbols.")
+
+    elif end_dt >= datetime.strptime("2017-08-24","%Y-%m-%d").date():
+        return (f"Your entry failed to return a search. Please check your entry and try again.<br/>"
+                f"Please refer to the set of instructions below:<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Enter a start date between the dates of '2010-01-01' and '2017-08-23'.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Dates must be in YYYY-MM-DD format.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Do not preceed or end the date with either  '  or  '""' symbols.")
+        
+    elif end_dt <= datetime.strptime("2009-12-31","%Y-%m-%d").date():
+        return (f"Your entry failed to return a search. Please check your entry and try again.<br/>"
+                f"Please refer to the set of instructions below:<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Enter a start date between the dates of '2010-01-01' and '2017-08-23'.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Dates must be in YYYY-MM-DD format.<br/>"
+                f"&nbsp&nbsp&nbsp&nbsp&nbsp* Do not preceed or end the date with either  '  or  '""' symbols.")
+    
+    else:
+        
+        session = Session(engine)
+    
+        temp_data = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+             filter(Measurement.date >= start_dt).\
+             filter(Measurement.date <= end_dt).all()
+
+        temp_data_between = []
+        for tmax, tavg, tmin in temp_data:
+            temp_dict_2 = {}
+            temp_dict_2["Temp (max)"] = tmax
+            temp_dict_2["Temp (avg)"] = tavg
+            temp_dict_2["Temp (min)"] = tmin
+
+            temp_data_between.append(temp_dict_2)
+
+        return jsonify(temp_data_between)
+
+
     
 if __name__ == '__main__':
     app.run(debug=True)
